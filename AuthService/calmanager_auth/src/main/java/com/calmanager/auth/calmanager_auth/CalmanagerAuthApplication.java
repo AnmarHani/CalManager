0b83d0cfecfe65ed4ea741282f8ca1dd258854f4
AnmarHani <AnmarHaniV1@gmail.com>
2025-08-09 14:04:54 +0300

    Initial Skeleton Commit

---
+++ AuthService/calmanager_auth/src/main/java/com/calmanager/auth/calmanager_auth/CalmanagerAuthApplication.java
@@ -0,0 +1,13 @@
+package com.calmanager.auth.calmanager_auth;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CalmanagerAuthApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CalmanagerAuthApplication.class, args);
+	}
+
+}
---
+++ AuthService/calmanager_auth/src/test/java/com/calmanager/auth/calmanager_auth/CalmanagerAuthApplicationTests.java
@@ -0,0 +1,13 @@
+package com.calmanager.auth.calmanager_auth;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CalmanagerAuthApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
